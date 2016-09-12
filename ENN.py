f1de7769359c7a08051fb70b4fbcefa691ebdcff
Faizaan Charania <faizaancharania@gmail.com>
2016-09-12 14:28:17 +0530

    get movie review classes

--- ENN.py
+++ ENN.py
@@ -5,8 +5,36 @@ import math
 #import test
 import csv
 import pandas
+import exec_db
+
+def getUserReviewClasses(movie_id=''):
+    class_1=[]
+    class_1=execQuery(query="select distinct user_id from reviews where review=1 and movie_id="+str(movie_id),dB= "top500.db")
+    for i in range(len(class_1)):
+        class_1[i]=list(class_1[i])[0]
 
 
+    class_2=[]
+    class_2=execQuery(query="select distinct user_id from reviews where review=2 and movie_id="+str(movie_id),dB= "top500.db")
+    for i in range(len(class_2)):
+        class_2[i]=list(class_2[i])[0]
+
+
+    class_3=[]
+    class_3=execQuery(query="select distinct user_id from reviews where review=3 and movie_id="+str(movie_id),dB= "top500.db")
+    for i in range(len(class_3)):
+        class_3[i]=list(class_3[i])[0]
+
+
+    class_4=[]
+    class_4=execQuery(query="select distinct user_id from reviews where review=4 and movie_id="+str(movie_id),dB= "top500.db")
+    for i in range(len(class_4)):
+        class_4[i]=list(class_4[i])[0]
+
+    class_5=[]
+    class_5=execQuery(query="select distinct user_id from reviews where review=5 and movie_id="+str(movie_id),dB= "top500.db")
+    for i in range(len(class_5)):
+        class_5[i]=list(class_5[i])[0]
 
 def getClassStatistics(user="",movie="",k=100):
     stats = [0 for x in range(5)]
