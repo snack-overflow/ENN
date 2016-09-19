2f547ad404165f8adf810b668e8b3659695675b6
Devashree Mehta <devashree.mehta95@gmail.com>
2016-09-20 00:26:11 +0530

    debugging enn.py

--- ENN_dup.py
+++ ENN_dup.py
@@ -6,40 +6,40 @@ import math
 import csv
 import pandas
 from exec_db import *
-# import pdb
-# pdb.set_trace()
+import pdb
+pdb.set_trace()
 def getUserReviewClasses(movie_id='', user=''):
    class_1=[]
    class_1=execQuery(query="select distinct user_id from reviews where review=1 and movie_id="+str(movie_id)+ " and user_id<>" + str(user),dB= "top500.db")
    for i in range(len(class_1)):
-       print i, " in for 1 of getUserReviewClasses"
+       #print i, " in for 1 of getUserReviewClasses"
        class_1[i]=list(class_1[i])[0]
 
 
    class_2=[]
    class_2=execQuery(query="select user_id from reviews where review=2 and movie_id="+str(movie_id)+ " and user_id<>" + str(user),dB= "top500.db")
    for i in range(len(class_2)):
-       print i, " in for 2 of getUserReviewClasses"
+       #print i, " in for 2 of getUserReviewClasses"
        class_2[i]=list(class_2[i])[0]
 
 
    class_3=[]
    class_3=execQuery(query="select user_id from reviews where review=3 and movie_id="+str(movie_id)+ " and user_id<>" + str(user),dB= "top500.db")
    for i in range(len(class_3)):
-       print i, " in for 3 of getUserReviewClasses"
+       #print i, " in for 3 of getUserReviewClasses"
        class_3[i]=list(class_3[i])[0]
 
 
    class_4=[]
    class_4=execQuery(query="select user_id from reviews where review=4 and movie_id="+str(movie_id)+ " and user_id<>" + str(user),dB= "top500.db")
    for i in range(len(class_4)):
-       print i, " in for 4 of getUserReviewClasses"
+       #print i, " in for 4 of getUserReviewClasses"
        class_4[i]=list(class_4[i])[0]
 
    class_5=[]
    class_5=execQuery(query="select user_id from reviews where review=5 and movie_id="+str(movie_id)+ " and user_id<>" + str(user),dB= "top500.db")
    for i in range(len(class_5)):
-       print i, " in for 5 of getUserReviewClasses"
+       #print i, " in for 5 of getUserReviewClasses"
        class_5[i]=list(class_5[i])[0]
 
    return class_1,class_2,class_3,class_4,class_5
@@ -50,7 +50,7 @@ def getCorrelation(user1='', user2=''):
    with open('allCorrelations.csv','rb') as f:
        all_corr=pandas.read_csv(f,header=None)
        for i in range(len(all_corr)):
-           print i, " in for of getCorrelation"
+           #print i, " in for of getCorrelation"
            if all_corr.ix[i][0]==int(user1) and all_corr.ix[i][1]==int(user2):
                corr=all_corr.ix[i][2]
            elif all_corr.ix[i][0]==int(user2) and all_corr.ix[i][1]==int(user1):
@@ -63,7 +63,7 @@ def getNearestNeighbours(user=''):
     with open('allCorrelations.csv','rb') as f:
         all_corr=pandas.read_csv(f,header=None)
         for i in range(len(all_corr)):
-            #print i, " in for of getNearestNeighbours"
+            ##print i, " in for of getNearestNeighbours"
             if all_corr.ix[i][0]==int(user):
                 #print [ all_corr.ix[i][1], all_corr.ix[i][2]]
                 results.append([ all_corr.ix[i][1], all_corr.ix[i][2]])
@@ -80,14 +80,14 @@ def getAllNearestNeighbours(class_1=[], class_2=[], class_3=[], class_4=[], clas
     class4_data = []
     class5_data = []
     for i in range(len(class_1)):
-        print i, " in for 1 of getAllNearestNeighbours"
+        #print i, " in for 1 of getAllNearestNeighbours"
         user_dict = {}
         user_dict['user'] = class_1[i]
         del_indexes = []
         allNeighbours = []
         allNeighbours = getNearestNeighbours(class_1[i])
         for i in range(len(allNeighbours)):
-            print i
+            #print i, " in for 11 of getAllNearestNeighbours"
             if allNeighbours[i][0] in class_1:
                 allNeighbours[i].append(1)
             elif allNeighbours[i][0] in class_2:
@@ -109,14 +109,14 @@ def getAllNearestNeighbours(class_1=[], class_2=[], class_3=[], class_4=[], clas
         class1_data.append(user_dict)
  
     for i in range(len(class_2)):
-        print i, " in for 2 of getAllNearestNeighbours"
+        #print i, " in for 2 of getAllNearestNeighbours"
         user_dict = {}
         user_dict['user'] = class_2[i]
         del_indexes = []
         allNeighbours = []
         allNeighbours = getNearestNeighbours(class_2[i])
         for i in range(len(allNeighbours)):
-            print i
+            #print i, " in for 22 of getAllNearestNeighbours"
             if allNeighbours[i][0] in class_1:
                 allNeighbours[i].append(1)
             elif allNeighbours[i][0] in class_2:
@@ -138,14 +138,14 @@ def getAllNearestNeighbours(class_1=[], class_2=[], class_3=[], class_4=[], clas
         class2_data.append(user_dict)
  
     for i in range(len(class_3)):
-        print i, " in for 3 of getAllNearestNeighbours"
+        #print i, " in for 3 of getAllNearestNeighbours"
         user_dict = {}
         user_dict['user'] = class_3[i]
         del_indexes = []
         allNeighbours = []
         allNeighbours = getNearestNeighbours(class_3[i])
         for i in range(len(allNeighbours)):
-            print i
+            #print i, " in for 33 of getAllNearestNeighbours"
             if allNeighbours[i][0] in class_1:
                 allNeighbours[i].append(1)
             elif allNeighbours[i][0] in class_2:
@@ -167,14 +167,14 @@ def getAllNearestNeighbours(class_1=[], class_2=[], class_3=[], class_4=[], clas
         class3_data.append(user_dict)
  
     for i in range(len(class_4)):
-        print i, " in for 4 of getAllNearestNeighbours"
+        #print i, " in for 4 of getAllNearestNeighbours"
         user_dict = {}
         user_dict['user'] = class_4[i]
         del_indexes = []
         allNeighbours = []
         allNeighbours = getNearestNeighbours(class_4[i])
         for i in range(len(allNeighbours)):
-            print i
+            #print i, " in for 44 of getAllNearestNeighbours"
             if allNeighbours[i][0] in class_1:
                 allNeighbours[i].append(1)
             elif allNeighbours[i][0] in class_2:
@@ -196,14 +196,14 @@ def getAllNearestNeighbours(class_1=[], class_2=[], class_3=[], class_4=[], clas
         class4_data.append(user_dict)
  
     for i in range(len(class_5)):
-        print i, " in for 5 of getAllNearestNeighbours"
+        #print i, " in for 5 of getAllNearestNeighbours"
         user_dict = {}
         user_dict['user'] = class_5[i]
         del_indexes = []
         allNeighbours = []
         allNeighbours = getNearestNeighbours(class_5[i])
         for i in range(len(allNeighbours)):
-            print i
+            #print i, " in for 55 of getAllNearestNeighbours"
             if allNeighbours[i][0] in class_1:
                 allNeighbours[i].append(1)
             elif allNeighbours[i][0] in class_2:
@@ -340,35 +340,35 @@ def getClassStatistics(class1_data = [],class2_data =[],class3_data=[],class4_da
     n = [0 for x in range(5)]
 
     for i in range(len(class1_data)):
-        print i, " in for 1 of getClassStatistics"
+        #print i, " in for 1 of getClassStatistics"
         allNeighbours = class1_data[i]['allNeighbours']
         for neighbour in allNeighbours:
             if neighbour[2] == 1:
-                stats[0]+=1
+                stats[0]+=1.0
     for i in range(len(class2_data)):
-        print i, " in for 2 of getClassStatistics"
+        #print i, " in for 2 of getClassStatistics"
         allNeighbours = class2_data[i]['allNeighbours']
         for neighbour in allNeighbours:
             if neighbour[2] == 2:
-                stats[1]+=1
+                stats[1]+=1.0
     for i in range(len(class3_data)):
-        print i, " in for 3 of getClassStatistics"
+        #print i, " in for 3 of getClassStatistics"
         allNeighbours = class3_data[i]['allNeighbours']
         for neighbour in allNeighbours:
             if neighbour[2] == 3:
-                stats[2]+=1
+                stats[2]+=1.0
     for i in range(len(class4_data)):
-        print i, " in for 4 of getClassStatistics"
+        #print i, " in for 4 of getClassStatistics"
         allNeighbours = class4_data[i]['allNeighbours']
         for neighbour in allNeighbours:
             if neighbour[2] == 4:
-                stats[3]+=1
+                stats[3]+=1.0
     for i in range(len(class5_data)):
-        print i, " in for 5 of getClassStatistics"
+        #print i, " in for 5 of getClassStatistics"
         allNeighbours = class5_data[i]['allNeighbours']
         for neighbour in allNeighbours:
             if neighbour[2] == 5:
-                stats[4]+=1
+                stats[4]+=1.0
 
     n[0] = len(class1_data)
     n[1] = len(class2_data)
@@ -376,7 +376,7 @@ def getClassStatistics(class1_data = [],class2_data =[],class3_data=[],class4_da
     n[3] = len(class4_data)
     n[4] = len(class5_data)
     for i in range(len(stats)):
-        print i, " in for last of getClassStatistics"
+        #print i, " in for last of getClassStatistics"
         stats[i]/=k
         if(n[i]==0):
             stats[i]=0
@@ -386,20 +386,39 @@ def getClassStatistics(class1_data = [],class2_data =[],class3_data=[],class4_da
 
 
 def ENN_main(user="", movie="", k= 100):
-    class_1,class_2,class_3, class_4, class_5 =  getUserReviewClasses(movie,user)
-
-    class1_data,class2_data,class3_data,class4_data,class5_data = getAllNearestNeighbours(class_1,class_2,class_3, class_4, class_5,k)
-    with open("class1_data","wb") as file:
-        pickle.dump(class1_data,file)
-    with open("class2_data","wb") as file:
-        pickle.dump(class2_data,file)
-    with open("class3_data","wb") as file:
-        pickle.dump(class3_data,file)
-    with open("class4_data","wb") as file:
-        pickle.dump(class4_data,file)
-    with open("class5_data","wb") as file:
-        pickle.dump(class5_data,file)
-
+    # class_1,class_2,class_3, class_4, class_5 =  getUserReviewClasses(movie,user)
+    # with open("class_1","wb") as file:
+    #     pickle.dump(class_1,file)
+    # with open("class_2","wb") as file:
+    #     pickle.dump(class_2,file)
+    # with open("class_3","wb") as file:
+    #     pickle.dump(class_3,file)
+    # with open("class_4","wb") as file:
+    #     pickle.dump(class_4,file)
+    # with open("class_5","wb") as file:
+    #     pickle.dump(class_5,file)
+    
+    # class1_data,class2_data,class3_data,class4_data,class5_data = getAllNearestNeighbours(class_1,class_2,class_3, class_4, class_5,k)
+    with open("class1_data","rb") as file:
+        class1_data = pickle.load(file)
+    with open("class2_data","rb") as file:
+        class2_data = pickle.load(file)
+    with open("class3_data","rb") as file:
+        class3_data = pickle.load(file)
+    with open("class4_data","rb") as file:
+        class4_data = pickle.load(file)
+    with open("class5_data","rb") as file:
+        class5_data = pickle.load(file)
+    with open("class_1","rb") as file:
+        class_1 = pickle.load(file)
+    with open("class_2","rb") as file:
+        class_2 = pickle.load(file)
+    with open("class_3","rb") as file:
+        class_3 = pickle.load(file)
+    with open("class_4","rb") as file:
+        class_4 = pickle.load(file)
+    with open("class_5","rb") as file:
+        class_5 = pickle.load(file)    
     ni=[len(class1_data),len(class2_data),len(class3_data),len(class4_data),len(class5_data)]
     rating_power = [0,0,0,0,0]
     stats=getClassStatistics(class1_data,class2_data,class3_data,class4_data,class5_data,k)
