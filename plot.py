d8ad76156a6705e08f0b5a10260c7219f360fed3
Faizaan Charania <faizaancharania@gmail.com>
2016-10-28 13:30:18 +0530

    Graph images

--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-plt.plot([50,100,250],[1.03537432844,1.05166534601,1.05546198416],'r',label='top 1000 users')
-plt.plot([50,100,250],[1.06583300756,1.06677082825,1.06957935657],'b',label='top 500 users')
+plt.plot([50,100,250],[0.995460214907, 1.00133482568,1.00373080872],'r',label='KNN')
+plt.plot([50,100,250],[0.97294750198,0.996532801,1.007698237],'b',label='ENN')
 plt.legend(loc='best')
 plt.xlabel("No. of neighbours (K)")
 plt.ylabel("RMSE")
-plt.axis([50,255,1.03,1.08])
-plt.title("RMSE with integer predictions")
+plt.axis([50,250,0.97,1.02])
+plt.title("KNN vs ENN (DB: top 500)")
 plt.show()
